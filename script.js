d0fdb86783579e1ee6140bfeec3d903f5441027b
mohamed-safouen <mohamedsafouen@gmail.com>
2021-11-07 03:40:40 +0100

    Add turn win condition Reset Game Same problem click function.

--- script.js
+++ script.js
@@ -1,48 +1,54 @@
 let tab = document.getElementById("table");
-const f=[];
-let y = 0;
-let z=document.getElementsByClassName("box");
+let f = new Array(8);
+let z = document.getElementsByClassName("box");
+let Player = document.getElementById("Player_turn")
+
 function grid() {
-    for (let i = 1; i < 9; i++) {
+    for (let i = 0; i < f.length; i++) {
         tab.innerHTML += '<div class="box"></div>';
     }
-    for (let i = 0; i < 3; i++) {
-        f[i] = [];
-        for (let j = 0; j < 3; j++) {
-            f[i][j] = [];
-
-
-        }
-    }
-
 }
 
-function turn(i,j) {
+let y = 0;
+Player.innerHTML = "This X Turn";
+
+function turn(i) {
     if (y == 0) {
-        f[i][j].innerHTML = "X";
+        Player.innerHTML = "This O Turn";
+        f[i] = "X";
         y = 1;
     } else {
-        f[i][j].innerHTML = "O";
+        Player.innerHTML = "This X Turn";
+        f[i] = "O";
         y = 0;
     }
-
+    return f[i]
 }
 
-
-
-
-
 // Function win conditions//
-function wincondition() {
+let winner = "";
+
+function wincondition(i) {
+
+    if ((winner == f[0] == f[1] == f[2]) || (winner == f[0] == f[3] == f[6]) || (winner == f[0] == f[4] == f[8]) ||
+        (winner == f[3] == f[4] == f[5]) || (winner == f[6] == f[7] == f[8]) || (winner == f[1] == f[4] == f[7]) ||
+        (winner == f[2] == f[5] == f[8]) || (winner == f[2] == f[4] == f[6])) {
+        if (winner == "X") {
+            alert("X win");
+            Player.innerHTML = "This O Turn";
+        } else {
+            alert("O win");
+        }
+    } else {
+        alert("Toe Game")
+    }
 }
 
 //reset function//
-function reset() {
 
-}
-function newgame(){
-    reset();
-    y=0;
+function newgame() {
+    f = [];
+    y = 0;
 }
 
 
