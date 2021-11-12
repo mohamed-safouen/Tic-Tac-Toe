a1ea4ff20a2556edb8b2c597b2372e138829a57a
mohamed-safouen <mohamedsafouen@gmail.com>
2021-11-12 02:27:41 +0100

    not yet

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
 let tab = document.getElementById("table");
-let f = new Array(9);
+let f = new Array();
 let Player = document.getElementById("Player_turn");
-z = document.getElementsByClassName("box");
+let z = document.createElement("span");
+let cross=document.createElement("div");
+cross.setAttribute("id","X");
+let circle=document.createElement("div");
+circle.setAttribute("id","O");
 
 function grid() {
-    for (let i = 0; i < f.length - 1; i++) {
+    for (let i = 0; i < 3; i++) {
         f[i] = [];
-        tab.innerHTML += '<div class="box" onclick="turn()" ></div>';
-        z[i].id = "div" + i;
-        mydiv = document.getElementById("div" + i);
-        mydiv.append(f[i])
+        for (let j=0;j<3;j++) {
+            f[i][j] = [];
+            z = document.createElement("span");
+            tab.append(z);
 
+        }
     }
+    return z
 }
-
+function click(i,j){
+    z[i][j].addEventListener("click",function() {
+        return turn();
+    });
+}
 let y = 0;
 Player.innerHTML = "This is X Turn";
-
-function turn(i) {
+function turn(i,j) {
     if (y == 0) {
         Player.innerHTML = "This is O Turn";
-        f[i] = "X";
-        document.getElementById("div" + i).innerHTML = f[i];
-        y = 1;
-    } else {
-        Player.innerHTML = "This is X Turn";
-        f[i] = "O";
-        mydiv.innerHTML = f[i];
-        y = 0;
+           f[i][j]="X" ;
+            y = 1;
+        } else {
+            Player.innerHTML = "This is X Turn";
+            f[i][j] = "O";
+            y = 0;
+        }
+        return (f[i][j]);
     }
-    return f[i]
-
-}
-
 
 // Function win conditions//
 let winner = "";
-
-function wincondition(i) {
+function wincondition(i,j) {
 
     if ((winner == f[0] == f[1] == f[2]) || (winner == f[0] == f[3] == f[6]) || (winner == f[0] == f[4] == f[8]) ||
         (winner == f[3] == f[4] == f[5]) || (winner == f[6] == f[7] == f[8]) || (winner == f[1] == f[4] == f[7]) ||
