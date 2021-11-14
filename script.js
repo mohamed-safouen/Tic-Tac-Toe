945414ba91f9d26af79a978aa7aef9362c26d664
mohamed-safouen <mohamedsafouen@gmail.com>
2021-11-14 01:34:05 +0100

    not yet

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 let tab = document.getElementById("table");
-let f = new Array();
+let f = new Array(3);
 let Player = document.getElementById("Player_turn");
-let z = document.createElement("span");
 let cross=document.createElement("div");
 cross.setAttribute("id","X");
 let circle=document.createElement("div");
 circle.setAttribute("id","O");
-
+z = document.createElement("span");
+let id =0
 function grid() {
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < f.length; i++) {
         f[i] = [];
-        for (let j=0;j<3;j++) {
-            f[i][j] = [];
+        for (let j=0;j<f.length;j++) {
             z = document.createElement("span");
+            z.setAttribute("id",id);
             tab.append(z);
+            f[i][j] = id;
+               id++
 
         }
-    }
-    return z
-}
-function click(i,j){
-    z[i][j].addEventListener("click",function() {
-        return turn();
-    });
+    }return id
 }
 let y = 0;
 Player.innerHTML = "This is X Turn";
-function turn(i,j) {
-    if (y == 0) {
-        Player.innerHTML = "This is O Turn";
-           f[i][j]="X" ;
-            y = 1;
-        } else {
-            Player.innerHTML = "This is X Turn";
-            f[i][j] = "O";
-            y = 0;
-        }
-        return (f[i][j]);
-    }
+ function turn(i,j) {
+
+             if (y == 0) {
+                 Player.innerHTML = "This is O Turn";
+                 f[i][j] = "X";
+                 y = 1;
+             } else {
+                 Player.innerHTML = "This is X Turn";
+                 f[i][j] = "O";
+                 y = 0;
+             }return f[i][j];
+         }
+
+
+
+
 
 // Function win conditions//
 let winner = "";
 function wincondition(i,j) {
 
-    if ((winner == f[0] == f[1] == f[2]) || (winner == f[0] == f[3] == f[6]) || (winner == f[0] == f[4] == f[8]) ||
-        (winner == f[3] == f[4] == f[5]) || (winner == f[6] == f[7] == f[8]) || (winner == f[1] == f[4] == f[7]) ||
-        (winner == f[2] == f[5] == f[8]) || (winner == f[2] == f[4] == f[6])) {
+    if ((winner == f[0][0] == f[0][1] == f[0][2]) || (winner == f[0][0] == f[1][0] == f[2][0]) || (winner == f[0][0] == f[1][1] == f[2][2]) ||
+        (winner == f[1][0] == f[1][1] == f[1][2]) || (winner == f[2][0] == f[2][1] == f[2][2]) || (winner == f[0][1] == f[1][1] == f[2][1]) ||
+        (winner == f[0][2] == f[1][2] == f[2][2]) || (winner == f[0][2] == f[1][1] == f[2][0])) {
         if (winner == "X") {
             alert("X win");
             Player.innerHTML = "Player X win";
@@ -55,7 +55,7 @@ function wincondition(i,j) {
         }
     } else {
         alert("Tie Game");
-        Player.innerHTML = "It's Toe Game";
+        Player.innerHTML = "It's Tie Game";
     }
 }
 
@@ -64,7 +64,7 @@ function wincondition(i,j) {
 function newgame() {
     f = new Array(8);
     y = 0;
-    Player.innerHTML = "This os X Turn";
+    Player.innerHTML = "This is X Turn";
 }
 
 
